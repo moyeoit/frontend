69dabe916081eeba37d424e3cae6533392fb4808
oaoong <pizzachicken@kakao.com>
2026-02-08 03:00:15 +0900

    fix: storybook 에러 해결

--- src/components/molecules/communityCard/communityCard.tsx
+++ src/components/molecules/communityCard/communityCard.tsx
@@ -3,7 +3,8 @@
 import * as React from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import { ThumbsUpEmptyIcon, MessageIcon } from '@/assets/icons'
+import { GreyMessage } from '@/assets/icons/GreyMessage'
+import { GreyThumbsUp } from '@/assets/icons/GreyThumbsUp'
 import useMediaQuery from '@/shared/hooks/useMediaQuery'
 import { resolveImageSrc } from '@/shared/utils'
 import { cn } from '@/shared/utils/cn'
@@ -191,13 +192,13 @@ export function CommunityCardMeta({
           <div className="flex items-center gap-1">
             {likes !== undefined && (
               <div className="flex items-center gap-1">
-                <ThumbsUpEmptyIcon className="text-grey-color-3 desktop:w-4 desktop:h-4 phone:w-3 phone:h-3" />
+                <GreyThumbsUp className="text-grey-color-3 desktop:w-4 desktop:h-4 phone:w-3 phone:h-3" />
                 <span>{likes}</span>
               </div>
             )}
             {comments !== undefined && (
               <div className="flex items-center gap-1">
-                <MessageIcon className="text-grey-color-3 desktop:w-4 desktop:h-4 phone:w-3 phone:h-3" />
+                <GreyMessage className="text-grey-color-3 desktop:w-4 desktop:h-4 phone:w-3 phone:h-3" />
                 <span>{comments}</span>
               </div>
             )}
